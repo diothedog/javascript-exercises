58feaec2bfcb8d125e7132757d58ad50984d76fa
diothedog <88989273+diothedog@users.noreply.github.com>
2024-05-23 16:45:39 -0500

    Pass first test of exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Split string into array of letters
+    const letters = string.split("");
+    // Initialize string to hold result
+    let reversed = ""
+    // Loop over each letter in array, starting from last index
+    for (let i = letters.length - 1; i >= 0; i--) {
+        reversed += letters[i];
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
