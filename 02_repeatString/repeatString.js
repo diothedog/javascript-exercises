79b31bf1b81bb3686474e2193f1c9ac748358e24
diothedog <88989273+diothedog@users.noreply.github.com>
2024-05-23 16:28:30 -0500

    Pass test 5 of exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    
     let repeated = ""
     for (let i = 0; i < num; i++) {
         repeated += string;
