8303e1fbb4a27e0b374cc666be55ee99c75aff1b
diothedog <88989273+diothedog@users.noreply.github.com>
2024-05-24 15:00:41 -0500

    Pass fifth test of exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
     let sum = 0;
     let bigger = Math.max(num1, num2);
     let smaller = Math.min(num1, num2);
