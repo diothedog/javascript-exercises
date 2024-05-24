93e86e44ae9657d0030015e23d5b36b3a312228f
diothedog <88989273+diothedog@users.noreply.github.com>
2024-05-24 15:48:39 -0500

    Pass all tests of exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,13 @@
 const convertToCelsius = function(fahrenheit) {
   let celsius = (fahrenheit - 32) * (5/9);
-  rounded = Math.round(celsius * 10) / 10;
+  let rounded = Math.round(celsius * 10) / 10;
   return rounded;
 };
 
 const convertToFahrenheit = function(celsius) {
   let fahrenheit = celsius * (9/5) + 32;
-  return fahrenheit;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
