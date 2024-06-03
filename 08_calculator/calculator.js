e0a353ac7866f324356f34fd82cbb6da7bcdf17b
diothedog <88989273+diothedog@users.noreply.github.com>
2024-06-03 14:54:47 -0500

    Pass test 12 of exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,15 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let product = 1;
+  for(let i = num; i < 0; i--) {
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line
