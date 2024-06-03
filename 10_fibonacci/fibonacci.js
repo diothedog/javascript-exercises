142284d95df0835b79483f2e0927e519fcf453d5
diothedog <88989273+diothedog@users.noreply.github.com>
2024-06-03 16:15:00 -0500

    Pass first test of exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let sequence = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        sequence[i] = sequence[i - 2] + sequence[i - 1];
+    }
+    return sequence[num];
 };
 
 // Do not edit below this line
