7f341efdc1a0a198e1572e8baad46485300fd510
diothedog <88989273+diothedog@users.noreply.github.com>
2024-06-03 14:41:43 -0500

    Pass test 11 of exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(arr) {
   return arr.reduce((total, currentValue) => total * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 const factorial = function() {
