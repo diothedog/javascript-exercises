269a674d6b38011f3547f13d55bb40400f096e51
diothedog <88989273+diothedog@users.noreply.github.com>
2024-06-04 14:16:13 -0500

    Pass test 2 of exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,21 @@
 const findTheOldest = function(people) {
     return people.reduce((oldest, current) => {
-        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
-        const currentAge = current.yearOfDeath - current.yearOfBirth;
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
         if (currentAge > oldestAge) {
             return current;
         } else {
             return oldest;
         }
     });
-
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
